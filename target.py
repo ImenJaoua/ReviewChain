04324bd07c40b714d4048d9e99fdf24fab637fab
jaouaime <jaouaime@ostende.iro.umontreal.ca>
2025-12-03 17:41:31 -0500

    chnage target

--- target.py
+++ target.py
@@ -1,218 +1,374 @@
-# vim: ft=python fileencoding=utf-8 sts=4 sw=4 et:
+import { EMPTY_OBJ, EMPTY_ARR } from '../constants';
+import { Component, enqueueRender } from '../component';
+import { coerceToVNode, Fragment } from '../create-element';
+import { diffChildren } from './children';
+import { diffProps } from './props';
+import { assign, removeNode } from '../util';
+import options from '../options';
 
-# Copyright 2014-2020 Florian Bruhin (The Compiler) <mail@qutebrowser.org>
-#
-# This file is part of qutebrowser.
-#
-# qutebrowser is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# qutebrowser is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with qutebrowser.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Diff two virtual nodes and apply proper changes to the DOM
+ * @param {import('../internal').PreactElement | Text} dom The DOM element representing
+ * the virtual nodes under diff
+ * @param {import('../internal').PreactElement} parentDom The parent of the DOM element
+ * @param {import('../internal').VNode | null} newVNode The new virtual node
+ * @param {import('../internal').VNode | null} oldVNode The old virtual node
+ * @param {object} context The current context object
+ * @param {boolean} isSvg Whether or not this element is an SVG node
+ * @param {Array<import('../internal').PreactElement>} excessDomChildren
+ * @param {Array<import('../internal').Component>} mounts A list of newly
+ * mounted components
+ * @param {import('../internal').Component | null} ancestorComponent The direct
+ * parent component
+ */
+export function diff(dom, parentDom, newVNode, oldVNode, context, isSvg, excessDomChildren, mounts, ancestorComponent, force) {
 
-"""Debugging console."""
+	// If the previous type doesn't match the new type we drop the whole subtree
+	if (oldVNode==null || newVNode==null || oldVNode.type!==newVNode.type) {
+		if (oldVNode!=null) unmount(oldVNode, ancestorComponent);
+		if (newVNode==null) return null;
+		dom = null;
+		oldVNode = EMPTY_OBJ;
+	}
 
-import sys
-import code
-import typing
+	if (options.diff) options.diff(newVNode);
 
-from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt
-from PyQt5.QtWidgets import QTextEdit, QWidget, QVBoxLayout, QApplication
-from PyQt5.QtGui import QTextCursor
+	let c, p, isNew = false, oldProps, oldState, snapshot,
+		newType = newVNode.type;
 
-from qutebrowser.config import stylesheet
-from qutebrowser.misc import cmdhistory, miscwidgets
-from qutebrowser.utils import utils, objreg
+	/** @type {import('../internal').Component | null} */
+	let clearProcessingException;
 
+	try {
+		outer: if (oldVNode.type===Fragment || newType===Fragment) {
+			diffChildren(parentDom, newVNode, oldVNode, context, isSvg, excessDomChildren, mounts, c);
 
-console_widget = None
+			if (newVNode._children.length) {
+				dom = newVNode._children[0]._dom;
+				newVNode._lastDomChild = newVNode._children[newVNode._children.length - 1]._dom;
+			}
+		}
+		else if (typeof newType==='function') {
 
+			// Necessary for createContext api. Setting this property will pass
+			// the context value as `this.context` just for this component.
+			let cxType = newType.contextType;
+			let isProvider = newType._id==='Provider';
+			let provider = cxType && context[cxType._id];
+			let cctx = cxType != null ? (provider ? provider.props.value : cxType._defaultValue) : context;
+			// Get component and set it to `c`
+			if (oldVNode._component) {
+				c = newVNode._component = oldVNode._component;
+				clearProcessingException = c._processingException;
+			}
+			else {
+				// Instantiate the new component
+				if (newType.prototype && newType.prototype.render) {
+					newVNode._component = c = new newType(newVNode.props, cctx); // eslint-disable-line new-cap
+				}
+				else {
+					newVNode._component = c = new Component(newVNode.props, cctx);
+					c.constructor = newType;
+					c.render = doRender;
+				}
+				c._ancestorComponent = ancestorComponent;
+				if (provider) provider.sub(c);
 
-class ConsoleLineEdit(miscwidgets.CommandLineEdit):
+				c.props = newVNode.props;
+				if (!c.state) c.state = {};
+				c.context = cctx;
+				c._context = context;
+				isNew = c._dirty = true;
+				c._renderCallbacks = [];
+			}
 
-    """A QLineEdit which executes entered code and provides a history.
+			c._vnode = newVNode;
 
-    Attributes:
-        _history: The command history of executed commands.
+			// Invoke getDerivedStateFromProps
+			let s = c._nextState || c.state;
+			if (newType.getDerivedStateFromProps!=null) {
+				oldState = assign({}, c.state);
+				if (s===c.state) s = c._nextState = assign({}, s);
+				assign(s, newType.getDerivedStateFromProps(newVNode.props, s));
+			}
 
-    Signals:
-        execute: Emitted when a commandline should be executed.
-    """
+			// Invoke pre-render lifecycle methods
+			if (isNew) {
+				if (newType.getDerivedStateFromProps==null && c.componentWillMount!=null) c.componentWillMount();
+				if (c.componentDidMount!=null) mounts.push(c);
+			}
+			else {
+				if (newType.getDerivedStateFromProps==null && force==null && c.componentWillReceiveProps!=null) {
+					c.componentWillReceiveProps(newVNode.props, cctx);
+					s = c._nextState || c.state;
+				}
 
-    execute = pyqtSignal(str)
+				if (!force && c.shouldComponentUpdate!=null && c.shouldComponentUpdate(newVNode.props, s, cctx)===false) {
+					c.props = newVNode.props;
+					c.state = s;
+					c._dirty = false;
+					break outer;
+				}
 
+				if (isProvider && c.props.value!==newVNode.props.value) {
+					const v = newVNode.props.value;
+					newVNode._component.subs.map(sub => v!==sub.context && (sub.context = v, enqueueRender(sub)));
+				}
 
-    def __init__(self, _namespace, parent):
-        """Constructor.
+				if (c.componentWillUpdate!=null) {
+					c.componentWillUpdate(newVNode.props, s, cctx);
+				}
+			}
 
-        Args:
-            _namespace: The local namespace of the interpreter.
-        """
-        super().__init__(parent=parent)
-        stylesheet.set_register(self)
-        self._history = cmdhistory.History(parent=self)
-        self.returnPressed.connect(self.on_return_pressed)
+			oldProps = c.props;
+			if (!oldState) oldState = c.state;
 
-    @pyqtSlot()
-    def on_return_pressed(self):
-        """Execute the line of code which was entered."""
-        self._history.stop()
-        text = self.text()
-        if text:
-            self._history.append(text)
-        self.execute.emit(text)
-        self.setText('')
+			c.context = cctx;
+			c.props = newVNode.props;
+			c.state = s;
 
-    def history_prev(self):
-        """Go back in the history."""
-        try:
-            if not self._history.is_browsing():
-                item = self._history.start(self.text().strip())
-            else:
-                item = self._history.previtem()
-        except (cmdhistory.HistoryEmptyError,
-                cmdhistory.HistoryEndReachedError):
-            return
-        self.setText(item)
+			if (options.render) options.render(newVNode);
 
-    def history_next(self):
-        """Go forward in the history."""
-        if not self._history.is_browsing():
-            return
-        try:
-            item = self._history.nextitem()
-        except cmdhistory.HistoryEndReachedError:
-            return
-        self.setText(item)
+			let prev = c._prevVNode;
+			let vnode = c._prevVNode = coerceToVNode(c.render(c.props, c.state, c.context));
+			c._dirty = false;
 
-    def keyPressEvent(self, e):
-        """Override keyPressEvent to handle special keypresses."""
-        if e.key() == Qt.Key_Up:
-            self.history_prev()
-            e.accept()
-        elif e.key() == Qt.Key_Down:
-            self.history_next()
-            e.accept()
-        elif e.modifiers() & Qt.ControlModifier and e.key() == Qt.Key_C:
-            self.setText('')
-            e.accept()
-        else:
-            super().keyPressEvent(e)
+			if (c.getChildContext!=null) {
+				context = assign(assign({}, context), c.getChildContext());
+			}
 
+			if (!isNew && c.getSnapshotBeforeUpdate!=null) {
+				snapshot = c.getSnapshotBeforeUpdate(oldProps, oldState);
+			}
 
-class ConsoleTextEdit(QTextEdit):
+			c.base = dom = diff(dom, parentDom, vnode, prev, context, isSvg, excessDomChildren, mounts, c, null);
 
-    """Custom QTextEdit for console output."""
+			if (vnode!=null) {
+				// If this component returns a Fragment (or another component that
+				// returns a Fragment), then _lastDomChild will be non-null,
+				// informing `diffChildren` to diff this component's VNode like a Fragemnt
+				newVNode._lastDomChild = vnode._lastDomChild;
+			}
 
-    STYLESHEET = """
-        ConsoleTextEdit {
-            font: {{ conf.fonts.debug_console }};
-        }
-    """
+			c._parentDom = parentDom;
 
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setAcceptRichText(False)
-        self.setReadOnly(True)
-        stylesheet.set_register(self)
-        self.setFocusPolicy(Qt.ClickFocus)
+			if (newVNode.ref) applyRef(newVNode.ref, c, ancestorComponent);
+		}
+		else {
+			dom = diffElementNodes(dom, newVNode, oldVNode, context, isSvg, excessDomChildren, mounts, ancestorComponent);
 
-    def __repr__(self):
-        return utils.get_repr(self)
+			if (newVNode.ref && (oldVNode.ref !== newVNode.ref)) {
+				applyRef(newVNode.ref, dom, ancestorComponent);
+			}
+		}
 
-    def append_text(self, text):
-        """Append new text and scroll output to bottom.
+		newVNode._dom = dom;
 
-        We can't use Qt's way to append stuff because that inserts weird
-        newlines.
-        """
-        self.moveCursor(QTextCursor.End)
-        self.insertPlainText(text)
-        scrollbar = self.verticalScrollBar()
-        scrollbar.setValue(scrollbar.maximum())
+		if (c!=null) {
+			while (p=c._renderCallbacks.pop()) p.call(c);
 
+			// Don't call componentDidUpdate on mount or when we bailed out via
+			// `shouldComponentUpdate`
+			if (!isNew && oldProps!=null && c.componentDidUpdate!=null) {
+				c.componentDidUpdate(oldProps, oldState, snapshot);
+			}
+		}
 
-class ConsoleWidget(QWidget):
+		if (clearProcessingException) {
+			c._processingException = null;
+		}
 
-    """A widget with an interactive Python console.
+		if (options.diffed) options.diffed(newVNode);
+	}
+	catch (e) {
+		catchErrorInComponent(e, ancestorComponent);
+	}
 
-    Attributes:
-        _lineedit: The line edit in the console.
-        _output: The output widget in the console.
-        _vbox: The layout which contains everything.
-        _more: A flag which is set when more input is expected.
-        _buffer: The buffer for multi-line commands.
-        _interpreter: The InteractiveInterpreter to execute code with.
-    """
+	return dom;
+}
 
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        if not hasattr(sys, 'ps1'):
-            sys.ps1 = '>>> '
-        if not hasattr(sys, 'ps2'):
-            sys.ps2 = '... '
-        namespace = {
-            '__name__': '__console__',
-            '__doc__': None,
-            'q_app': QApplication.instance(),
-            # We use parent as self here because the user "feels" the whole
-            # console, not just the line edit.
-            'self': parent,
-            'objreg': objreg,
-        }
-        self._more = False
-        self._buffer = []  # type: typing.MutableSequence[str]
-        self._lineedit = ConsoleLineEdit(namespace, self)
-        self._lineedit.execute.connect(self.push)
-        self._output = ConsoleTextEdit()
-        self.write(self._curprompt())
-        self._vbox = QVBoxLayout()
-        self._vbox.setSpacing(0)
-        self._vbox.addWidget(self._output)
-        self._vbox.addWidget(self._lineedit)
-        self.setLayout(self._vbox)
-        self._lineedit.setFocus()
-        self._interpreter = code.InteractiveInterpreter(namespace)
+export function commitRoot(mounts, root) {
+	let c;
+	while ((c = mounts.pop())) {
+		try {
+			c.componentDidMount();
+		}
+		catch (e) {
+			catchErrorInComponent(e, c._ancestorComponent);
+		}
+	}
 
-    def __repr__(self):
-        return utils.get_repr(self, visible=self.isVisible())
+	if (options.commit) options.commit(root);
+}
 
-    def write(self, line):
-        """Write a line of text (without added newline) to the output."""
-        self._output.append_text(line)
+/**
+ * Diff two virtual nodes representing DOM element
+ * @param {import('../internal').PreactElement} dom The DOM element representing
+ * the virtual nodes being diffed
+ * @param {import('../internal').VNode} newVNode The new virtual node
+ * @param {import('../internal').VNode} oldVNode The old virtual node
+ * @param {object} context The current context object
+ * @param {boolean} isSvg Whether or not this DOM node is an SVG node
+ * @param {*} excessDomChildren
+ * @param {Array<import('../internal').Component>} mounts An array of newly
+ * mounted components
+ * @param {import('../internal').Component} ancestorComponent The parent
+ * component to the ones being diffed
+ * @returns {import('../internal').PreactElement}
+ */
+function diffElementNodes(dom, newVNode, oldVNode, context, isSvg, excessDomChildren, mounts, ancestorComponent) {
+	let d = dom;
 
-    @pyqtSlot(str)
-    def push(self, line):
-        """Push a line to the interpreter."""
-        self._buffer.append(line)
-        source = '\n'.join(self._buffer)
-        self.write(line + '\n')
-        # We do two special things with the context managers here:
-        #   - We replace stdout/stderr to capture output. Even if we could
-        #     override InteractiveInterpreter's write method, most things are
-        #     printed elsewhere (e.g. by exec). Other Python GUI shells do the
-        #     same.
-        #   - We disable our exception hook, so exceptions from the console get
-        #     printed and don't open a crashdialog.
-        with utils.fake_io(self.write), utils.disabled_excepthook():
-            self._more = self._interpreter.runsource(source, '<console>')
-        self.write(self._curprompt())
-        if not self._more:
-            self._buffer = []
+	// Tracks entering and exiting SVG namespace when descending through the tree.
+	isSvg = newVNode.type==='svg' || isSvg;
 
-    def _curprompt(self):
-        """Get the prompt which is visible currently."""
-        return sys.ps2 if self._more else sys.ps1
+	if (dom==null && excessDomChildren!=null) {
+		for (let i=0; i<excessDomChildren.length; i++) {
+			const child = excessDomChildren[i];
+			if (child!=null && (newVNode.type===null ? child.nodeType===3 : child.localName===newVNode.type)) {
+				dom = child;
+				excessDomChildren[i] = null;
+				break;
+			}
+		}
+	}
 
+	if (dom==null) {
+		dom = newVNode.type===null ? document.createTextNode(newVNode.text) : isSvg ? document.createElementNS('http://www.w3.org/2000/svg', newVNode.type) : document.createElement(newVNode.type);
 
-def init():
-    """Initialize a global console."""
-    global console_widget
-    console_widget = ConsoleWidget()
+		// we created a new parent, so none of the previously attached children can be reused:
+		excessDomChildren = null;
+	}
+	newVNode._dom = dom;
+
+	if (newVNode.type===null) {
+		if ((d===null || dom===d) && newVNode.text!==oldVNode.text) {
+			dom.data = newVNode.text;
+		}
+	}
+	else {
+		if (excessDomChildren!=null && dom.childNodes!=null) {
+			excessDomChildren = EMPTY_ARR.slice.call(dom.childNodes);
+		}
+		if (newVNode!==oldVNode) {
+			let oldProps = oldVNode.props;
+			let newProps = newVNode.props;
+
+			// if we're hydrating, use the element's attributes as its current props:
+			if (oldProps==null) {
+				oldProps = {};
+				if (excessDomChildren!=null) {
+					let name;
+					for (let i=0; i<dom.attributes.length; i++) {
+						name = dom.attributes[i].name;
+						oldProps[name=='class' && newProps.className ? 'className' : name] = dom.attributes[i].value;
+					}
+				}
+			}
+			let oldHtml = oldProps.dangerouslySetInnerHTML;
+			let newHtml = newProps.dangerouslySetInnerHTML;
+			if (newHtml || oldHtml) {
+				// Avoid re-applying the same '__html' if it did not changed between re-render
+				if (!newHtml || !oldHtml || newHtml.__html!=oldHtml.__html) {
+					dom.innerHTML = newHtml && newHtml.__html || '';
+				}
+			}
+			if (newProps.multiple) {
+				dom.multiple = newProps.multiple;
+			}
+			diffChildren(dom, newVNode, oldVNode, context, newVNode.type==='foreignObject' ? false : isSvg, excessDomChildren, mounts, ancestorComponent);
+			diffProps(dom, newProps, oldProps, isSvg);
+		}
+	}
+
+	return dom;
+}
+
+/**
+ * Invoke or update a ref, depending on whether it is a function or object ref.
+ * @param {object|function} [ref=null]
+ * @param {any} [value]
+ */
+export function applyRef(ref, value, ancestorComponent) {
+	try {
+		if (typeof ref=='function') ref(value);
+		else ref.current = value;
+	}
+	catch (e) {
+		catchErrorInComponent(e, ancestorComponent);
+	}
+}
+
+/**
+ * Unmount a virtual node from the tree and apply DOM changes
+ * @param {import('../internal').VNode} vnode The virtual node to unmount
+ * @param {import('../internal').Component} ancestorComponent The parent
+ * component to this virtual node
+ * @param {boolean} skipRemove Flag that indicates that a parent node of the
+ * current element is already detached from the DOM.
+ */
+export function unmount(vnode, ancestorComponent, skipRemove) {
+	let r;
+	if (options.unmount) options.unmount(vnode);
+
+	if (r = vnode.ref) {
+		applyRef(r, null, ancestorComponent);
+	}
+
+	if (!skipRemove && vnode._lastDomChild==null && (skipRemove = ((r = vnode._dom)!=null))) removeNode(r);
+
+	vnode._dom = vnode._lastDomChild = null;
+
+	if ((r = vnode._component)!=null) {
+		if (r.componentWillUnmount) {
+			try {
+				r.componentWillUnmount();
+			}
+			catch (e) {
+				catchErrorInComponent(e, ancestorComponent);
+			}
+		}
+
+		r.base = r._parentDom = null;
+		if (r = r._prevVNode) unmount(r, ancestorComponent, skipRemove);
+	}
+	else if (r = vnode._children) {
+		for (let i = 0; i < r.length; i++) {
+			unmount(r[i], ancestorComponent, skipRemove);
+		}
+	}
+}
+
+/** The `.render()` method for a PFC backing instance. */
+function doRender(props, state, context) {
+	return this.constructor(props, context);
+}
+
+/**
+ * Find the closest error boundary to a thrown error and call it
+ * @param {object} error The thrown value
+ * @param {import('../internal').Component} component The first ancestor
+ * component check for error boundary behaviors
+ */
+function catchErrorInComponent(error, component) {
+	for (; component; component = component._ancestorComponent) {
+		if (!component._processingException) {
+			try {
+				if (component.constructor.getDerivedStateFromError!=null) {
+					component.setState(component.constructor.getDerivedStateFromError(error));
+				}
+				else if (component.componentDidCatch!=null) {
+					component.componentDidCatch(error);
+				}
+				else {
+					continue;
+				}
+				return enqueueRender(component._processingException = component);
+			}
+			catch (e) {
+				error = e;
+			}
+		}
+	}
+	throw error;
+}
