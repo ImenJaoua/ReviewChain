0ce9ed345a5f4f861cbe93a9444b77913d9e8090
jaouaime <jaouaime@ostende.iro.umontreal.ca>
2025-12-04 06:00:51 -0500

    add new target

--- target.py
+++ target.py
@@ -569,6 +569,8 @@ class Node(PlenumNode, HasPoolManager):
          client requests it.
         """
         result = reply.result
+
+        
         if result[TXN_TYPE] in (SCHEMA, ISSUER_KEY):
             result = deepcopy(result)
             result[DATA] = jsonSerz.serialize(result[DATA], toBytes=False)
