cb3723dac52323351cac2fa6e3d154b7c595dad7
Kasra Raoufi <73kpcl@gmail.com>
2025-12-18 20:24:58 -0500

    tool updatede

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="cmtcheck",
-    version="1.0",
+    name="reviewchain",
+    version="2.0",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
-            "cmtcheck = cmtcheck:main",
+            "reviewchain = reviewchain:main",
         ]
     },
-    py_modules=["cmtcheck", "cmtcheck_main", "chat_env",
+    py_modules=["reviewchain", "reviewchain_main", "chat_env",
                  "phase", "memory", "agents_local","comment_prompt",
                  "ref_prompt", "comment_prompt_AUTOMAT", "comment_prompt_chain_zero",
                  "comment_prompt_chain_few", "comment_prompt_Meta"],
